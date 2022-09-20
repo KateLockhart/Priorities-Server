0d599d332d77c0061d4adf7e0013c7dcdaa768a1
KateLockhart <katelockhart707@gmail.com>
2022-09-20 14:48:48 -0400

    Delete User route created, tested, and sucessfully removing data from db.

--- controllers/userController.js
+++ controllers/userController.js
@@ -97,5 +97,19 @@ router.put("/:id", async (req, res) => {
 });
 
 // TODO: Create a .delete to delete user data
+router.delete("/:id", async (req, res) => {
+  const User = await UserModel.findOne({ where: { id: req.params.id } });
+
+  try {
+    await User.destroy();
+    res.status(200).json({
+      message: `User account deleted. User ID ${User.dataValues.id} removed from database.`,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Unable to delete user. Error: ${err}.`,
+    });
+  }
+});
 
 module.exports = router;

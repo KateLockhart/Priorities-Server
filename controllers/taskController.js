8fb24121f8f0f06bd91d1178c33a953b274af29b
KateLockhart <katelockhart707@gmail.com>
2022-09-21 10:51:04 -0400

    Create Task post route complete, tested, and persisting to db.

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,10 +1,34 @@
 const router = require("express").Router();
 const { TaskModel } = require("../models");
 
-// TODO: Create instance with post method
+router.post("/", async (req, res) => {
+  const { title, description, importanceRating, urgencyRating, completed } =
+    req.body.task;
+
+  try {
+    const Task = await TaskModel.create({
+      title: title,
+      description: description,
+      importanceRating: importanceRating,
+      urgencyRating: urgencyRating,
+      completed: completed,
+    });
+
+    res.status(201).json({
+      message: "Task successfully created.",
+      task: Task,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Failed to create task in database. Error: ${err}.`,
+    });
+  }
+});
 
 // TODO: Read instance with get method
 
 // TODO: Update instance with put method
 
 // TODO: Delete instance with delete method
+
+module.exports = router;

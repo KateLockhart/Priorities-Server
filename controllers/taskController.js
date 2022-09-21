4047e83a7013ce6533ae6a0c396e7958af25a156
Kate Lockhart <63356074+KateLockhart@users.noreply.github.com>
2022-09-21 12:03:00 -0400

    Task controller CRUD complete.
    * Created base task controller file, added task routing in app.js,  and included task exports in models and controllers index.js.
    
    * Create Task post route complete, tested, and persisting to db.
    
    * Read Task get all functionality complete, tested, and implimenting.
    
    * Read Task get single task complete, tested, and funtional.
    
    * Update Task put funtionality complete, tested, and persisting to db.
    
    * Delete Task request complete, tested, and functional.

---
+++ controllers/taskController.js
@@ -0,0 +1,110 @@
+const router = require("express").Router();
+const { TaskModel } = require("../models");
+
+router.post("/", async (req, res) => {
+  const { title, description, importanceRating, urgencyRating, completed } =
+    req.body.task;
+
+  try {
+    const Task = await TaskModel.create({
+      title: title,
+      description: description,
+      importanceRating: importanceRating,
+      urgencyRating: urgencyRating,
+      completed: completed,
+    });
+
+    res.status(201).json({
+      message: "Task successfully created.",
+      task: Task,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Failed to create task in database. Error: ${err}.`,
+    });
+  }
+});
+
+// TODO: Change to not use route param, use validateJWT once constructed, final route of "/"
+router.get("/all/:userId", async (req, res) => {
+  try {
+    const Tasks = await TaskModel.findAll({
+      // TODO: where value will change to { userId: id }
+      where: { userId: null },
+    });
+
+    res.status(200).json({
+      message: "All tasks found in database.",
+      tasks: Tasks,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Failed to locate tasks in database. Error: ${err}.`,
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const Task = await TaskModel.findOne({
+      where: { id: req.params.id },
+    });
+
+    res.status(200).json({
+      message: "Task successfully found in database.",
+      task: Task,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Failed to locate task in database. Error: ${err}.`,
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { title, description, importanceRating, urgencyRating, completed } =
+    req.body.task;
+
+  const Task = await TaskModel.findOne({
+    where: { id: req.params.id },
+  });
+
+  const updateTask = {
+    title: title,
+    description: description,
+    importanceRating: importanceRating,
+    urgencyRating: urgencyRating,
+    completed: completed,
+  };
+
+  Task.set(updateTask);
+
+  try {
+    const updateTask = await Task.save();
+    res.status(201).json({
+      message: "Task updated successfully.",
+      updateTask,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Failed to update task in database. Error: ${err}.`,
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const Task = await TaskModel.findOne({ where: { id: req.params.id } });
+
+  try {
+    await Task.destroy();
+    res.status(200).json({
+      message: `Task deleted. Task ID ${Task.dataValues.id} removed from database.`,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Unable to delete task. Error: ${err}.`,
+    });
+  }
+});
+
+module.exports = router;

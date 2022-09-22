140f48a6eb544071ced38599fc80e1200eded4ba
KateLockhart <katelockhart707@gmail.com>
2022-09-22 17:45:57 -0400

    Route in app.js and export in models/index.js created for categoryController.

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const dbConnection = require("./db");
-
 const controllers = require("./controllers");
 
 app.use(express.json());
 
 app.use("/user", controllers.userController);
 app.use("/task", controllers.taskController);
+app.use("/category", controllers.categoryController);
 
 dbConnection
   .authenticate()

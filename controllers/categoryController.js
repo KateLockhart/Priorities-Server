e4faa45df60cbbe2c799b982e707d7f8044ff740
KateLockhart <katelockhart707@gmail.com>
2022-09-22 17:59:11 -0400

    Create Category post request built, tested, and functionally persisting to db.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,2 +1,32 @@
 const router = require("express").Router();
 const { CategoryModel } = require("../models");
+
+router.post("/", async (req, res) => {
+  const { title, color } = req.body.category;
+
+  try {
+    const Category = await CategoryModel.create({
+      title: title,
+      color: color,
+    });
+
+    res.status(201).json({
+      message: "Category successfully created.",
+      category: Category,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Failed to create category in database. Error ${err}.`,
+    });
+  }
+});
+
+// TODO: Read all instances of categories with get
+
+// TODO: Read single instance of category with get
+
+// TODO: Update instance of category with put
+
+// TODO: Delete instance of category with delete
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   userController: require("./userController"),
   taskController: require("./taskController"),
+  categoryController: require("./categoryController"),
 };

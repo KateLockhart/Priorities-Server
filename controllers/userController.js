d3889022049fb4c75c69a50969b2395dfa6f30b2
KateLockhart <katelockhart707@gmail.com>
2022-09-28 14:04:50 -0400

    User login post route constructed, testing, and working. Use of bcrypt and JWT fully functional.

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,10 +48,48 @@ router.post("/", async (req, res) => {
   }
 });
 
-// TODO: Create .post for user login
-// router.post("/login", async ( req, res ) => {
+router.post("/login", async (req, res) => {
+  let { email, password } = req.body.user;
 
-// })
+  try {
+    let loginUser = await UserModel.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (loginUser) {
+      let passwordComparison = await bcrypt.compare(
+        password,
+        loginUser.password
+      );
+
+      if (passwordComparison) {
+        let token = jwt.sign({ id: loginUser.id }, process.env.JWT_SECRET, {
+          expiresIn: 60 * 60 * 24,
+        });
+
+        res.status(200).json({
+          user: loginUser,
+          message: "User successfully logged in.",
+          sessionToken: token,
+        });
+      } else {
+        res.status(401).json({
+          message: "Incorrect email or password",
+        });
+      }
+    } else {
+      res.status(401).json({
+        message: "Incorrect email or password",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to login user.",
+    });
+  }
+});
 
 router.get("/:id", async (req, res) => {
   try {
